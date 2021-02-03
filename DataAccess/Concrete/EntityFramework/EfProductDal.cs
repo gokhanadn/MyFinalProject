1d0bf0bc01ea54b5c7b3badb82f13338d075320a
Gökhan Aydın <aydingo18@itu.edu.tr>
2021-02-03 23:54:46 +0300

    03.02.2021 Ders Sonu

---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    //NuGet
+    public class EfProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            // IDisposable pattern implementation of c#
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var addedEntity = context.Entry(entity); // Referansı yakala.
+                addedEntity.State = EntityState.Added; // Ekleme işlemini yap.
+                context.SaveChanges(); // Değişikleri kaydet.
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var deletedEntity = context.Entry(entity); // Referansı yakala.
+                deletedEntity.State = EntityState.Deleted; // Silme işlemini yap.
+                context.SaveChanges(); // Değişikleri kaydet.
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return filter == null ? context.Set<Product>().ToList() : context.Set<Product>().Where(filter).ToList();
+
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                var updatedEntity = context.Entry(entity); // Referansı yakala.
+                updatedEntity.State = EntityState.Modified; // Güncelleme işlemini yap.
+                context.SaveChanges(); // Değişikleri kaydet.
+            }
+        }
+    }
+}
